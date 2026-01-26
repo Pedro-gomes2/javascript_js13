4e1780cdcc87ec9906adbac7521b619160d6d86b
Joao <oliveira110965@gmail.com>
2026-01-26 11:29:05 -0300

    Projeto HelloWorld

---
+++ helloword/HelloWord.js
@@ -0,0 +1,8 @@
+const leia = require("readline-sync") 
+
+let numero1=leia.questionInt("Digite o 1 numero: ");
+let numero2=leia.questionInt("Digite o 2 numero: ");
+
+let soma= numero1+numero2;
+
+console.log("Soma= "+soma);
